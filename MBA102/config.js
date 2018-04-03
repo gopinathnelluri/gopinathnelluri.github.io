38a2ea500b8d50a454fcf5545b799179113e493e
Gopinath Nelluri <gopinathnelluri@hotmail.com>
2018-04-02 21:46:36 -0400

    portfolio update

--- MBA102/config.js
+++
@@ -1,1 +0,0 @@
-IDRViewer.config = {"pagecount":5,"title":"","author":"Rubina Upreti","fileName":"MBA102.pdf","bounds":[[908,1286],[908,1286],[908,1286],[908,1286],[908,1286]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
